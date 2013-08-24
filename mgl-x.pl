75165ed4f59ba20490a5bb416c4a4043d6492f19
Nikita Pichugin <pichuginn@gmail.com>
2013-08-24 13:00:12 +0400

    mgl-x.pl: legacy code was removed and autostart of the "nvidia-settings" was commented out (it should be configured within a WM). README was slightly updated to reflect these changes.

--- mgl-x.pl
+++ mgl-x.pl
@@ -2,34 +2,12 @@
 
 use strict;
 use warnings;
-#use FindBin;
-
-### Legacy code below
-
-#my ($fontW, $fontH) = (7, 14);
-#my ($screenW, $screenH);
-#
-#foreach my $line (`/usr/bin/xdpyinfo`)
-#{
-#    if ($line =~ /^\s*dimensions:\s*(\d+)x(\d+)/)
-#    {
-#        my $scrollbarW = 12;
-#        $screenW = int( ($1 - $scrollbarW) / $fontW );
-#        $screenH = int( $2 / $fontH );
-#        last;
-#    }
-#}
-#
-#my $term = "/usr/bin/urxvt +hold -sr -fn ${fontW}x${fontH} -geometry ${screenW}x${screenH}+0+0 -fg gray50 -bg black -e";
-#my $mglpath = "$FindBin::Bin/mgl.pl";
 
 my $display = 1;
 while (-e "/tmp/.X11-unix/X${display}")
 {
     $display ++;
 }
-
-#system("/usr/bin/startx ${term} \"${mglpath}\" -- \":${display}\" &");
 
 my $wm = shift @ARGV;
 if (not defined $wm)
@@ -38,10 +16,13 @@ if (not defined $wm)
 }
 system("/usr/bin/startx ${wm} -- \":${display}\" &");
 
-while (not -e "/tmp/.X11-unix/X${display}")
-{
-    sleep 1;
-}
+# The following thing should be configured within a WM
+# If you don't use a WM, you may wish to uncomment this block of code
 
-system("DISPLAY=\":${display}\" /usr/bin/nvidia-settings -l");
+#while (not -e "/tmp/.X11-unix/X${display}")
+#{
+#    sleep 1;
+#}
+#
+#system("DISPLAY=\":${display}\" /usr/bin/nvidia-settings -l");
 
